37cd3d786887cda129f7ae2690b3e3bc9a441d73
Damon Oehlman <damon.oehlman@gmail.com>
2013-10-15 19:20:26 +1100

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# rtc-streamclass
+
+This is a simple parser that takes a string of text and determines what
+that means in the context of WebRTC.
---
+++ index.js
@@ -0,0 +1,10 @@
+/* jshint node: true */
+'use strict';
+
+/**
+  # rtc-streamclass
+
+  This is a simple parser that takes a string of text and determines what
+  that means in the context of WebRTC.
+
+**/
