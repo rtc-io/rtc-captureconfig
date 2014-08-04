532c6a0dc1020cec48c42a72c082b0242bf0b1cd
Damon Oehlman <damon.oehlman@gmail.com>
2014-08-04 12:50:43 +1000

    Update test to check for required new format

--- test/camera-fps-constraints.js
+++ test/camera-fps-constraints.js
@@ -1,36 +1,49 @@
 var test = require('tape');
 var expect = require('./helpers/expect-constraints');
 
+test('camera 15fps', expect({
+  audio: true,
+  video: {
+    mandatory: {},
+
+    optional: [
+      { minFrameRate: 15 },
+      { maxFrameRate: 15 },
+      { frameRate: 15 }
+    ]
+  }
+}));
+
 test('camera max:15fps', expect({
   audio: true,
   video: {
-    mandatory: {
-      maxFrameRate: 15
-    },
-
-    optional: []
+    mandatory: {},
+    optional: [
+      { maxFrameRate: 15 },
+      { frameRate: { max: 15 } }
+    ]
   }
 }));
 
 test('camera min:25fps', expect({
   audio: true,
   video: {
-    mandatory: {
-      minFrameRate: 25
-    },
-
-    optional: []
+    mandatory: {},
+    optional: [
+      { minFrameRate: 25 },
+      { frameRate: { min: 25 } }
+    ]
   }
 }));
 
 test('camera min:15fps max:25fps', expect({
   audio: true,
   video: {
-    mandatory: {
-      minFrameRate: 15,
-      maxFrameRate: 25
-    },
-
-    optional: []
+    mandatory: {},
+    optional: [
+      { minFrameRate: 15 },
+      { maxFrameRate: 25 },
+      { frameRate: { min: 15, max: 25 } }
+    ]
   }
 }));
