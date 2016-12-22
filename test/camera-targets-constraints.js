540dd50dcd5e860d7f93af6a1cbd116c7db00bf2
Oehlman <nathan.oehlman@gmail.com>
2016-12-22 12:01:46 +1000

    Add some more tests around constraints and sources

--- test/camera-targets-constraints.js
+++ test/camera-targets-constraints.js
@@ -8,6 +8,24 @@ test('camera', expect({ video: true, audio: true }, format.STANDARD));
 test('camera:1', expect({ video: true, audio: true }, format.LEGACY));
 test('camera:1', expect({ video: true, audio: true }, format.STANDARD));
 
+test('camera:1 min:320x240', expect({
+	video: {
+		mandatory: {},
+	    optional: [
+	      { minWidth: 320 },
+	      { minHeight: 240 }
+	    ]
+	},
+	audio: true
+}, format.LEGACY));
+test('camera:1 min:320x240', expect({
+	video: {
+	    width: { min: 320 },
+	    height: { min: 240 }
+	},
+	audio: true
+}, format.STANDARD));
+
 
 test('microphone', expect({ video: false, audio: true }, format.LEGACY));
 test('microphone', expect({ video: false, audio: true }, format.STANDARD));
--- test/camera-targets.js
+++ test/camera-targets.js
@@ -10,3 +10,18 @@ test('microphone:1', expect({ microphone: 1 }));
 // camera + microphone
 test('camera microphone:1', expect({ camera: true, microphone: 1 }));
 test('camera:1 microphone:2', expect({ camera: 1, microphone: 2 }));
+
+test('camera min:320x240 microphone:1', expect({
+	camera: true,
+	microphone: 1,
+	res: {
+		min: { w: 320, h: 240 }
+	}
+}));
+test('camera:1 ideal:640x480 microphone:2', expect({
+	camera: 1,
+	microphone: 2,
+	res: {
+		ideal: { w: 640, h: 480 }
+	}
+}));
